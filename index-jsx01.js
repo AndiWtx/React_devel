a4eb1b29a87a7efdff51d62101edbed274c8b8c3
AndiWtx <108346940+AndiWtx@users.noreply.github.com>
2023-02-08 00:29:56 +0200

    Delete index-jsx01.js

--- index-jsx01.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-let hello = <h1>Hello, World</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(hello);
-
-
-
