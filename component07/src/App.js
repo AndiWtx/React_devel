171fa34ca14f592c85bbfa628f0cce2226717c8c
Andrei <andywatta05@gmail.com>
2023-02-10 23:25:12 +0200

    part7

---
+++ component07/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelloWorld } from './HelloWorld';
+
+const helloWorldElement = <HelloWorld />
+
+export class App extends React.Component {
+  render() {
+    return <div>{helloWorldElement}</div>;
+  }
+}
+
+let aplication = <App />
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(aplication);
+
+
