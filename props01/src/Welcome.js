6a49081fdf732f886a1e9e8c9f20977b6017214e
Andrei <andywatta05@gmail.com>
2023-02-11 00:17:58 +0200

    prop-1

---
+++ props01/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+export class Welcome extends React.Component {
+  render() {
+    return <p>Welcome, {this.props.name}!</p>
+  }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render();
+
+
