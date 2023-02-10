8968154eb75b9d08f27bc55436186c8d328c7c71
Andrei <andywatta05@gmail.com>
2023-02-10 16:10:35 +0200

    comp3 fix

--- component03/src/index.js
+++ component03/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HelloWorld from './App';
 
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
 
 
 class App extends React.Component {
