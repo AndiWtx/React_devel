bea7f1742502e9d4dcdec2764c58c2bb148fb5dd
Andrei <andywatta05@gmail.com>
2023-02-16 01:48:13 +0200

    events01

---
+++ events01/src/HandleButtonClick.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export class HandleButtonClick extends React.Component {
+  state = { count: 0 }
+
+  handleCounterIncrement() {
+    this.setState(state => {
+      return {
+        count: state.count + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleCounterIncrement.bind(this)}>
+          Increment
+        </button>
+        <p>Count: {this.state.count}</p>
+      </>
+    )
+  }
+
+}
+
