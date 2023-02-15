f7acaeb06570c77a68600cfb1df1d5bf1b48d55f
Andrei <andywatta05@gmail.com>
2023-02-15 03:09:32 +0200

    state4

---
+++ state04/src/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { CounterDisplay } from './CounterDisplay';
+
+export class Counter extends React.Component {
+  state = {
+    count: 0
+  }
+
+  constructor(props) {
+    super(props)
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + 1
+        }
+      })
+    }, 1000)
+  }
+
+
+
+  render() {
+    return <CounterDisplay count={this.state.count} />
+  }
+
+}
+
