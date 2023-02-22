1b3602c77a906a0bd445bb1cbf539efa5413facf
Andrei <andywatta05@gmail.com>
2023-02-23 01:59:54 +0200

    style01

---
+++ styling01/src/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class Welcome extends React.Component {
+  render() {
+    return <h1 className='welcome-comp'> Welcome to the styling page!</h1>
+  }
+}
---
+++ styling01/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Welcome } from './Welcome.js'
+import './index.css'
+
+
+const root = document.querySelector('#root')
+ReactDOM.render(<Welcome />, root);
