89382588ea992a2b4100ed96180399ea5535800f
Andrei <andywatta05@gmail.com>
2023-02-22 02:08:37 +0200

    lists04

---
+++ lists04/src/ToDoList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export class TodoList extends React.Component {
+  state = {
+    item: "",
+    items: ["Things To Do"],
+  };
+
+  handleItemsAddition = (e) =>
+    this.setState({ item: "", items: [...this.state.items, this.state.item] });
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="item"
+          value={this.state.item}
+          onChange={(e) => this.setState({ item: e.target.value })}
+        />
+        <button onClick={this.handleItemsAddition}>Add Item</button>
+
+        <ul>
+          {this.state.items.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
